a0afec6daa5373f4ef7fd1d3db8716b3e0fd69d1
Q Chen <q.chen2@outlook.com>
2024-10-03 08:06:55 +0800

    Make some variables private

--- lru.py
+++ lru.py
@@ -30,14 +30,14 @@ class LRUTemplate:
     def __init__(self, size: int, tab_size_ratio: float = 3.0, value_dtype: Any = nb.int64):
         """value_dtype can be any numpy struct/dtype. maybe also jitclass object (not tested)"""
         self.size = size
-        self.cur_size: int = 0
+        self._cur_size: int = 0
         self.head: int = -2  ## **index** of head node
         self.tail: int = -2
-        self.tab_size: int = int(np.ceil(size * tab_size_ratio))
+        self._tab_size: int = int(np.ceil(size * tab_size_ratio))
         ## this must be 2lines, otherwise numba won't compile bc type infered as int64
-        self.table = np.ones((self.tab_size, 3), dtype=np.int32)
-        self.table *= -2
-        self.value = -2 * np.ones(self.tab_size, dtype=value_dtype)
+        self._table = np.ones((self._tab_size, 3), dtype=np.int32)
+        self._table *= -2
+        self._value = -2 * np.ones(self._tab_size, dtype=value_dtype)
         self.__debug_perf_find_calls = 0
         self.__debug_perf_find_hops = 0
         self.__debug_perf_recycle_calls = 0
@@ -58,7 +58,7 @@ class LRUTemplate:
             self.__debug_perf_recycle_calls,
         )
 
-    def _find_slot(self, key, get_free_only=0):
+    def _find_slot(self, key: int, get_free_only: bool = 0):
         """Returns (idx_match, idx_best).
         Some possible returns (idx always >= 0):
             * idx1, idx2 >= 0 :
@@ -69,16 +69,16 @@ class LRUTemplate:
         """
         self.__debug_perf_find_calls += 1
         idx_match = idx_best = -1
-        idx = myhash(key) % self.tab_size
+        idx = myhash(key) % self._tab_size
         init_idx = idx  ## for debugging only
         end_reached_once = 0
         hops = 0  ## for debug
-        while hops < 2 * self.tab_size:
+        while hops < 2 * self._tab_size:
             hops += 1
             if get_free_only and idx_best >= 0:
                 return -1, idx_best
             self.__debug_perf_find_hops += 1
-            key_cur = self.table[idx, 0]
+            key_cur = self._table[idx, 0]
             if key_cur >= 0:  ## slot is taken
                 if key_cur == key:
                     idx_match = idx
@@ -91,31 +91,31 @@ class LRUTemplate:
                 else:
                     return -1, idx_best
             idx += 1
-            if idx >= self.tab_size:
+            if idx >= self._tab_size:
                 if end_reached_once:
                     return -1, idx_best
                 end_reached_once = 1
                 idx = 0
         raise Exception("This should be unreachable")
 
-    def _move(self, from_, to):
+    def _move(self, from_: int, to: int):
         """Move node from index from_ to index to."""
         # print(f'__DEBUG move from {from_} to {to}.')
-        self.value[to] = self.value[from_]
-        self.table[to, :] = self.table[from_, :]
-        prev, next = self.table[to, 1], self.table[to, 2]
+        self._value[to] = self._value[from_]
+        self._table[to, :] = self._table[from_, :]
+        prev, next = self._table[to, 1], self._table[to, 2]
         # self.table[from_, 0] = -1
-        self.table[from_, 0] = -2  ## debug
+        self._table[from_, 0] = -2  ## debug
         if prev >= 0:
-            self.table[prev, 2] = to
+            self._table[prev, 2] = to
         if next >= 0:
-            self.table[next, 1] = to
+            self._table[next, 1] = to
         if self.head == from_:
             self.head = to
         if self.tail == from_:
             self.tail = to
 
-    def get(self, key, move_head=1):
+    def get(self, key: int, move_head: bool = 1):
         """Returns the key by value if found or -2 if not found"""
         idx_match, idx_best = self._find_slot(key)
         if idx_match < 0:
@@ -125,11 +125,11 @@ class LRUTemplate:
                 self._move(idx_match, idx_best)
             if self.head != idx_best:
                 self._make_head(idx_best)
-            return self.value[idx_best]
+            return self._value[idx_best]
         else:
-            return self.value[idx_match]
+            return self._value[idx_match]
 
-    def put(self, key, value, check_match=1):
+    def put(self, key: int, value, check_match: bool = 1):
         """Put new <key, value> pair in the hashtable.
 
         Args:
@@ -143,100 +143,87 @@ class LRUTemplate:
             if idx_best != idx_match:
                 self._move(idx_match, idx_best)
             ## replace the values and make the node head
-            self.value[idx_best] = value
+            self._value[idx_best] = value
             if self.head != idx_best:
                 self._make_head(idx_best)
         else:  ## no match found, need to insert new node
-            if self.cur_size == self.size:
+            if self._cur_size == self.size:
                 ## pop the old tail, cur_size - 1
                 old_tail = self.tail
-                old_tail_prev = self.table[old_tail, 1]
+                old_tail_prev = self._table[old_tail, 1]
                 self.tail = old_tail_prev
-                self.table[self.tail, 2] = -2
+                self._table[self.tail, 2] = -2
                 self._pop(old_tail)
-                self.cur_size -= 1
+                self._cur_size -= 1
                 ## recycling changes position, so need to find idx_best again
                 _, idx_best = self._find_slot(key, get_free_only=True)
             ## insert the new node, make head
-            self.table[idx_best, 0] = key
-            self.table[idx_best, 1] = -2
-            self.value[idx_best] = value
+            self._table[idx_best, 0] = key
+            self._table[idx_best, 1] = -2
+            self._value[idx_best] = value
             old_head = self.head
             if old_head < 0:  ## the cache is still empty
                 self.tail = idx_best
             else:
-                self.table[idx_best, 2] = old_head  ## new_node.next = old_head
-                self.table[old_head, 1] = idx_best  ## old_head.prev = new_node
+                self._table[idx_best, 2] = old_head  ## new_node.next = old_head
+                self._table[old_head, 1] = idx_best  ## old_head.prev = new_node
             self.head = idx_best
-            self.cur_size += 1
+            self._cur_size += 1
 
-    def _make_head(self, node):
+    def _make_head(self, node: int):
         ## node must not be head already
         old_head = self.head
 
         if node == self.tail:
-            self.tail = self.table[node, 1]
-            self.table[self.tail, 2] = -2
+            self.tail = self._table[node, 1]
+            self._table[self.tail, 2] = -2
         else:
             # match_node.next.prev = match_node.prev
-            node_next = self.table[node, 2]
-            self.table[node_next, 1] = self.table[node, 1]
+            node_next = self._table[node, 2]
+            self._table[node_next, 1] = self._table[node, 1]
 
         # match_node.prev.next = match_node.next
-        node_prev = self.table[node, 1]
-        self.table[node_prev, 2] = self.table[node, 2]
-        self.table[node, 2] = old_head
-        self.table[old_head, 1] = node
+        node_prev = self._table[node, 1]
+        self._table[node_prev, 2] = self._table[node, 2]
+        self._table[node, 2] = old_head
+        self._table[old_head, 1] = node
         self.head = node
-        self.table[node, 1] = -2
+        self._table[node, 1] = -2
 
-    def _pop(self, node):
+    def _pop(self, node: int):
         """Pop the node with no-tombstone method. Assumes the node is occupied."""
         self.__debug_perf_recycle_calls += 1
 
         i = j = node
         while True:
             self.__debug_perf_recycle_hops += 1
-            j = (j + 1) % self.tab_size
-            if self.table[j, 0] == -2:
+            j = (j + 1) % self._tab_size
+            if self._table[j, 0] == -2:
                 break
-            k = myhash(self.table[j, 0]) % self.tab_size
+            k = myhash(self._table[j, 0]) % self._tab_size
             if (j > i and (k <= i or k > j)) or (j < i and (k <= i and k > j)):
                 self._move(j, i)
                 i = j
-        self.table[i, 0] = -2
+        self._table[i, 0] = -2
 
 
 def make_lru(value_dtype: Any = nb.int64):
     _lru_jit_spec = [
         ("size", nb.int64),
-        ("cur_size", nb.int64),
+        ("_cur_size", nb.int64),
         ("head", nb.int64),
         ("tail", nb.int64),
         ("tab_size_ratio", nb.float64),
-        ("tab_size", nb.int64),
-        ("table", nb.int32[:, :]),  ## np.iinfo('int32').max = 2_147_483_647
-        ("value", value_dtype[:]),
+        ("_tab_size", nb.int64),
+        ("_table", nb.int32[:, :]),  ## np.iinfo('int32').max = 2_147_483_647
+        ("_value", value_dtype[:]),
         ("__debug_perf_find_calls", nb.int64),
         ("__debug_perf_find_hops", nb.int64),
         ("__debug_perf_recycle_calls", nb.int64),
         ("__debug_perf_recycle_hops", nb.int64),
     ]
 
-    cls = nb.experimental.jitclass(_lru_jit_spec)(
-        LRUTemplate
-    )  # (size, tab_size_ratio, value_dtype)
-    # JITTED[value_dtype] = cls
-    # return JITTED[value_dtype](size, tab_size_ratio)
-    # _ = cls(50, 3, value_dtype)
-    # return cls(size, tab_size_ratio, value_dtype)
-
-    # def get_jitted(*args, **kwargs):
-    #     kwargs['value_dtype'] = value_dtype
-    #     # return JITTED[value_dtype](*args, **kwargs)
-    #     return cls(*args, **kwargs)
-
-    # return get_jitted
+    cls = nb.experimental.jitclass(_lru_jit_spec)(LRUTemplate)
     return cls
 
 
